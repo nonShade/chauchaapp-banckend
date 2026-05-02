708e5e3d4c4d8295df437fd8349e84ea6070f501
Ainsiel <c.pavez07@ufromail.cl>
2026-05-02 08:43:06 -0400

    qa: add transactions data

--- scripts/seed-qa.sql
+++ scripts/seed-qa.sql
@@ -178,6 +178,53 @@ INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id
 SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Salud'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 85000.00, 'Dentista', '2026-02-15'
 FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
 
+-- NEW DATA FOR QA (MAY & HEAVY APRIL)
+-- Current Month: May 2026
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Ingreso'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Sueldo'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Mensual'), 850000.00, 'Sueldo Mayo', '2026-05-01'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Vivienda'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Mensual'), 350000.00, 'Arriendo Mayo', '2026-05-05'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Alimentación'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 55000.00, 'Supermercado Mayo', '2026-05-03'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Transporte'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 15000.00, 'Carga Bip Mayo', '2026-05-07'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Servicios Básicos'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Mensual'), 42000.00, 'Luz y Agua Mayo', '2026-05-10'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+-- Heavy Expenses Month: April 2026
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Vivienda'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Mensual'), 350000.00, 'Arriendo Abril', '2026-04-05'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Salud'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 150000.00, 'Consulta Médica Especialista', '2026-04-08'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Educación'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 200000.00, 'Curso Desarrollo Web', '2026-04-12'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Entretenimiento'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 95000.00, 'Cena Aniversario', '2026-04-18'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Otros Gastos'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 60000.00, 'Compra Imprevista', '2026-04-22'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
+INSERT INTO "transaction" (user_id, transaction_type_id, transaction_category_id, transaction_frequency_id, amount, description, transaction_date)
+SELECT u.user_id, (SELECT transaction_type_id FROM transaction_type WHERE name = 'Gasto'), (SELECT transaction_category_id FROM transaction_category WHERE name = 'Alimentación'), (SELECT transaction_frequency_id FROM transaction_frequency WHERE name = 'Única'), 70000.00, 'Supermercado Extra Abril', '2026-04-25'
+FROM "user" u WHERE u.email = 'test_login@chauchaapp.cl' ON CONFLICT DO NOTHING;
+
 
 -- ============================================================
 -- QA SEED COMPLETE
